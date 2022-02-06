f42329345703800e5941b713a3de2ef11f49b92b
Steve C <diceroll123@gmail.com>
2022-02-05 20:32:28 -0500

    Change indentation to width: 4
    thank heavens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-  Box,
-  VStack,
-  Grid,
-  Divider,
-  useToast,
-  SimpleGrid,
+    Box,
+    VStack,
+    Grid,
+    Divider,
+    useToast,
+    SimpleGrid,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import axios from 'axios';
@@ -17,147 +17,147 @@ import { EMOTIONS, SIZES } from './utils/constants';
 
 
 function App() {
-  const [petName, setPetName] = React.useState('');
-  const [alreadySavedPets, setAlreadySavedPets] = React.useState([]);
-  const [canDownload, setCanDownload] = React.useState(false);
-  const toast = useToast();
+    const [petName, setPetName] = React.useState('');
+    const [alreadySavedPets, setAlreadySavedPets] = React.useState([]);
+    const [canDownload, setCanDownload] = React.useState(false);
+    const toast = useToast();
 
-  const addPetToState = (petName, error) => {
-    setAlreadySavedPets(existingArray => {
-      const petIndex = existingArray.findIndex(pet => pet.petName.toLowerCase() === petName.toLowerCase());
-      const newPet = {
-        error,
-        petName,
-        downloaded: 0,
-        done: false,
-      };
-      return [newPet, ...existingArray.filter((_, i) => i !== petIndex)]
-    });
-  };
+    const addPetToState = (petName, error) => {
+        setAlreadySavedPets(existingArray => {
+            const petIndex = existingArray.findIndex(pet => pet.petName.toLowerCase() === petName.toLowerCase());
+            const newPet = {
+                error,
+                petName,
+                downloaded: 0,
+                done: false,
+            };
+            return [newPet, ...existingArray.filter((_, i) => i !== petIndex)]
+        });
+    };
 
-  const updatePetInState = (petName, data) => {
-    setAlreadySavedPets(existingArray => {
-      const petIndex = Math.max(0, existingArray.findIndex(pet => pet.petName.toLowerCase() === petName.toLowerCase()));
-      const existingPet = existingArray[petIndex];
-      const newPet = { // a neoPet, in a way
-        ...existingPet,
-        ...data,
-      };
-      return [...existingArray.slice(0, petIndex), newPet, ...existingArray.slice(petIndex + 1)]
-    });
-  };
+    const updatePetInState = (petName, data) => {
+        setAlreadySavedPets(existingArray => {
+            const petIndex = Math.max(0, existingArray.findIndex(pet => pet.petName.toLowerCase() === petName.toLowerCase()));
+            const existingPet = existingArray[petIndex];
+            const newPet = { // a neoPet, in a way
+                ...existingPet,
+                ...data,
+            };
+            return [...existingArray.slice(0, petIndex), newPet, ...existingArray.slice(petIndex + 1)]
+        });
+    };
 
-  const incrementDownloadedForPet = (petName) => {
-    setAlreadySavedPets(existingArray => {
-      const petIndex = Math.max(0, existingArray.findIndex(pet => pet.petName.toLowerCase() === petName.toLowerCase()));
-      const existingPet = existingArray[petIndex];
-      const newPet = {
-        ...existingPet,
-        downloaded: (existingPet?.downloaded ?? 0) + 1,
-      };
-      return [...existingArray.slice(0, petIndex), newPet, ...existingArray.slice(petIndex + 1)]
-    });
-  };
+    const incrementDownloadedForPet = (petName) => {
+        setAlreadySavedPets(existingArray => {
+            const petIndex = Math.max(0, existingArray.findIndex(pet => pet.petName.toLowerCase() === petName.toLowerCase()));
+            const existingPet = existingArray[petIndex];
+            const newPet = {
+                ...existingPet,
+                downloaded: (existingPet?.downloaded ?? 0) + 1,
+            };
+            return [...existingArray.slice(0, petIndex), newPet, ...existingArray.slice(petIndex + 1)]
+        });
+    };
 
-  const makeZip = async (name, sci) => {
-    let zipWriter = new zip.ZipWriter(new zip.BlobWriter("application/zip"));
+    const makeZip = async (name, sci) => {
+        let zipWriter = new zip.ZipWriter(new zip.BlobWriter("application/zip"));
 
-    let promises = []; // whee
-    let error = undefined;
+        let promises = []; // whee
+        let error = undefined;
 
-    try {
-      for (const [emo_name, emo_value] of Object.entries(EMOTIONS)) {
-        for (const [size_name, size_value] of Object.entries(SIZES)) {
-          promises.push(
-            fetch(`http://localhost:8080/http://pets.neopets.com/cp/${sci}/${emo_value}/${size_value}.png`)
-              .then(async (response) => {
-                const path = `${size_name}/${emo_name}.png`;
-                const blob = await response.blob();
-                await zipWriter.add(path, new zip.BlobReader(blob))
-                incrementDownloadedForPet(name);
-              })
-          );
-        };
-      }
-      await Promise.all(promises); // Grab 'em all!
-    } catch (e) {
-      error = e;
-      toast({
-        id: name,
-        status: 'error',
-        title: `Error downloading ${name}'s images: ${e}`,
-        isClosable: true
-      });
-    }
+        try {
+            for (const [emo_name, emo_value] of Object.entries(EMOTIONS)) {
+                for (const [size_name, size_value] of Object.entries(SIZES)) {
+                    promises.push(
+                        fetch(`http://localhost:8080/http://pets.neopets.com/cp/${sci}/${emo_value}/${size_value}.png`)
+                            .then(async (response) => {
+                                const path = `${size_name}/${emo_name}.png`;
+                                const blob = await response.blob();
+                                await zipWriter.add(path, new zip.BlobReader(blob))
+                                incrementDownloadedForPet(name);
+                            })
+                    );
+                };
+            }
+            await Promise.all(promises); // Grab 'em all!
+        } catch (e) {
+            error = e;
+            toast({
+                id: name,
+                status: 'error',
+                title: `Error downloading ${name}'s images: ${e}`,
+                isClosable: true
+            });
+        }
 
-    const dataURI = URL.createObjectURL(await zipWriter.close());
+        const dataURI = URL.createObjectURL(await zipWriter.close());
 
-    const anchor = document.createElement("a");
-    const clickEvent = new MouseEvent("click");
-    anchor.href = dataURI;
-    anchor.download = `${name}-${sci}.zip`;
-    anchor.dispatchEvent(clickEvent);
-    URL.revokeObjectURL(dataURI);
+        const anchor = document.createElement("a");
+        const clickEvent = new MouseEvent("click");
+        anchor.href = dataURI;
+        anchor.download = `${name}-${sci}.zip`;
+        anchor.dispatchEvent(clickEvent);
+        URL.revokeObjectURL(dataURI);
 
-    updatePetInState(petName, { error, done: true });
-  };
+        updatePetInState(petName, { error, done: true });
+    };
 
-  const getSci = async (petName) => {
-    if (petName === "" || !canDownload) { return; }
-    try {
-      // TODO: use fetch for this request + remove axios dependency
-      addPetToState(petName, false);
-      setPetName("");
-      const response = await axios.head(`http://localhost:8080/http://pets.neopets.com/cpn/${petName}/1/1.png`);
-      await makeZip(petName, response.headers['x-final-url'].split('/')[4]);
+    const getSci = async (petName) => {
+        if (petName === "" || !canDownload) { return; }
+        try {
+            // TODO: use fetch for this request + remove axios dependency
+            addPetToState(petName, false);
+            setPetName("");
+            const response = await axios.head(`http://localhost:8080/http://pets.neopets.com/cpn/${petName}/1/1.png`);
+            await makeZip(petName, response.headers['x-final-url'].split('/')[4]);
 
-    } catch (error) {
-      toast({
-        id: 'getSci',
-        status: 'error',
-        title: `Error downloading ${petName}'s images - make sure you spelled their name correctly.`,
-        isClosable: true
-      });
-      updatePetInState(petName, { error: true });
-    }
-  };
+        } catch (error) {
+            toast({
+                id: 'getSci',
+                status: 'error',
+                title: `Error downloading ${petName}'s images - make sure you spelled their name correctly.`,
+                isClosable: true
+            });
+            updatePetInState(petName, { error: true });
+        }
+    };
 
-  const handlePetNameChange = (event) => {
-    let name = event.target.value;
-    setPetName(name);
-    setCanDownload(false);
-  };
+    const handlePetNameChange = (event) => {
+        let name = event.target.value;
+        setPetName(name);
+        setCanDownload(false);
+    };
 
-  return (
-    <Box textAlign="center" fontSize="xl">
-      <Grid p={3} >
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8} divider={<Divider maxW='3xl' />}>
+    return (
+        <Box textAlign="center" fontSize="xl">
+            <Grid p={3} >
+                <ColorModeSwitcher justifySelf="flex-end" />
+                <VStack spacing={8} divider={<Divider maxW='3xl' />}>
 
-          <About />
+                    <About />
 
-          <EnterNeopetName
-            petName={petName}
-            setCanDownload={setCanDownload}
-            handlePetNameChange={handlePetNameChange}
-            getSci={getSci}
-          />
+                    <EnterNeopetName
+                        petName={petName}
+                        setCanDownload={setCanDownload}
+                        handlePetNameChange={handlePetNameChange}
+                        getSci={getSci}
+                    />
 
-          <SimpleGrid columns={3} spacing={4} minWidth={'xl'}>
-            {alreadySavedPets.map(({ error, petName, downloaded, done }) => (
-              <SavedPet
-                error={error}
-                petName={petName}
-                downloaded={downloaded}
-                done={done}
-              />
-            ))}
-          </SimpleGrid>
+                    <SimpleGrid columns={3} spacing={4} minWidth={'xl'}>
+                        {alreadySavedPets.map(({ error, petName, downloaded, done }) => (
+                            <SavedPet
+                                error={error}
+                                petName={petName}
+                                downloaded={downloaded}
+                                done={done}
+                            />
+                        ))}
+                    </SimpleGrid>
 
-        </VStack>
-      </Grid>
-    </Box>
-  );
+                </VStack>
+            </Grid>
+        </Box>
+    );
 }
 
 export default App;
