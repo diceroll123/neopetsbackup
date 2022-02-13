ad9f5830778166f87ddd8cf30f164c119acf0ec7
Steve C <diceroll123@gmail.com>
2022-02-12 20:50:32 -0500

    Add default color mode (dark) but use system setting first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import { 
-  ChakraProvider, 
+import {
+  ChakraProvider,
   ColorModeScript,
-  theme,
 } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import theme from "./theme";
 
 ReactDOM.render(
   <StrictMode>
-    <ColorModeScript />
+    <ColorModeScript initialColorMode="dark" useSystemColorMode="true" />
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
