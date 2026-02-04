cd6622eb8fedf4de88ea9744842933730b2f2d79
Steve C <diceroll123@gmail.com>
2026-02-03 19:07:18 -0500

    fix themes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import {
-  ChakraProvider,
-  ColorModeScript,
-} from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
-import theme from "./theme";
+import theme from './theme';
 
 ReactDOM.render(
   <StrictMode>
-    <ColorModeScript initialColorMode="dark" useSystemColorMode="true" />
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
